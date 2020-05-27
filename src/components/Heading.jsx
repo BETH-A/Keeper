b84945e51f7bb4dbd52e81592a8863fde975b82c
Beth-A <elizabeth78@me.com>
2020-05-27 13:33:35 -0400

    Upload app Frontend

---
+++ src/components/Heading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DynamicFeedIcon from '@material-ui/icons/DynamicFeed';
+
+function Heading(){
+    return (
+    <header>
+        <h1>
+            <DynamicFeedIcon />
+            Keeper</h1>
+    </header>)
+};
+
+export default Heading;
